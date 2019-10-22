f8d1c6771184ee26bf37b0b2f121526a2320081e
JimmyPhong16 <JimmyPhong16@csu.fullerton.edu>
2019-10-22 14:46:28 -0700

    added output feature

--- ThreadSynchronization/Source.cpp
+++ ThreadSynchronization/Source.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include<Windows.h>
+#include <fstream>
 
 
 using namespace std;
 HANDLE mutex;
 
 DWORD WINAPI printNum(LPVOID lpParam) {
+	ofstream outFile;
+	outFile.open("synch.txt", std::ios_base::app); // this will continue to update synch.txt file
 	//outer loop for amount of lines
 	for (int a = 0; a < 20; a++) 
 	{
@@ -13,29 +16,35 @@ DWORD WINAPI printNum(LPVOID lpParam) {
 		//inner loop for how many numbers per line
 		for (int b = 1; b <= 26; b++)
 		{
+			outFile << b << " ";
 			cout << b << " ";
 		}
+		outFile << endl;
 		cout << endl;
 	}
 	ReleaseMutex(mutex);
 	cout << "number printing complete" << endl;
+	outFile.close();
 	return 0;
 }
 DWORD WINAPI printAlpha(LPVOID lpParam) {
-
+	ofstream outFile;
+	outFile.open("synch.txt", std::ios_base::app);
 	for (int a = 0; a < 20; a++)
 	{
 		WaitForSingleObject(mutex, INFINITE);
 		for (int b = 65; b <= 90; b++)
 		{
 			cout << (char)b << " ";
+			outFile << (char)b << " ";
 		}
 		cout << endl;
+		outFile << endl;
 	}
 	ReleaseMutex(mutex);
 	cout << "alphabet printing complete" << endl;
+	outFile.close();
 	return 0;
-
 }
 int main()
 {
@@ -47,14 +56,6 @@ int main()
 		FALSE,
 		NULL);
 
-	numThread = CreateThread(
-		NULL,
-		0,
-		printNum,
-		NULL,
-		0,
-		NULL);
-
 	alphaThread = CreateThread(
 		NULL,
 		0,
@@ -63,8 +64,19 @@ int main()
 		0,
 		NULL);
 
-	WaitForSingleObject(numThread, INFINITE);
+	numThread = CreateThread(
+		NULL,
+		0,
+		printNum,
+		NULL,
+		0,
+		NULL);
+
+	
+	
 	WaitForSingleObject(alphaThread, INFINITE);
+	WaitForSingleObject(numThread, INFINITE);
+	
 
 	CloseHandle(numThread);
 	CloseHandle(alphaThread);
