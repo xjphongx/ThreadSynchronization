44397847cd1934f563106e6b872e1c99afd52b44
JimmyPhong16 <JimmyPhong16@csu.fullerton.edu>
2019-10-24 21:42:54 -0700

    Simplified the threading structure to include the thread library

--- ThreadSynchronization/Source.cpp
+++ ThreadSynchronization/Source.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include<Windows.h>
 #include <fstream>
+#include <thread>
+#include <mutex>
 
 
-using namespace std;
-HANDLE mutex;
+using namespace std; 
+mutex mtx;
 
-DWORD WINAPI printNum(LPVOID lpParam) {
+void printNum() {
+	mtx.lock();
 	ofstream outFile;
 	outFile.open("synch.txt", std::ios_base::app); // this will continue to update synch.txt file
 	//outer loop for amount of lines
 	for (int a = 0; a < 20; a++) 
 	{
-		WaitForSingleObject(mutex, INFINITE);
 		//inner loop for how many numbers per line
 		for (int b = 1; b <= 26; b++)
 		{
@@ -22,17 +24,16 @@ DWORD WINAPI printNum(LPVOID lpParam) {
 		outFile << endl;
 		cout << endl;
 	}
-	ReleaseMutex(mutex);
 	cout << "number printing complete" << endl;
 	outFile.close();
-	return 0;
+	mtx.unlock();
 }
-DWORD WINAPI printAlpha(LPVOID lpParam) {
+void printAlpha() {
+	mtx.lock();
 	ofstream outFile;
 	outFile.open("synch.txt", std::ios_base::app);
 	for (int a = 0; a < 20; a++)
 	{
-		WaitForSingleObject(mutex, INFINITE);
 		for (int b = 65; b <= 90; b++)
 		{
 			cout << (char)b << " ";
@@ -41,45 +42,21 @@ DWORD WINAPI printAlpha(LPVOID lpParam) {
 		cout << endl;
 		outFile << endl;
 	}
-	ReleaseMutex(mutex);
 	cout << "alphabet printing complete" << endl;
 	outFile.close();
-	return 0;
+	mtx.unlock();
 }
 int main()
 {
+	ofstream outFile;
 	cout << "Thread Synchronizaton" << endl;
-	HANDLE numThread, alphaThread;
+	thread numThread(printNum);
+	thread alphaThread(printAlpha);
 
-	mutex = CreateMutex(
-		NULL,
-		FALSE,
-		NULL);
-
-	alphaThread = CreateThread(
-		NULL,
-		0,
-		printAlpha,
-		NULL,
-		0,
-		NULL);
-
-	numThread = CreateThread(
-		NULL,
-		0,
-		printNum,
-		NULL,
-		0,
-		NULL);
-
+	numThread.join();
+	alphaThread.join();
 	
 	
-	WaitForSingleObject(alphaThread, INFINITE);
-	WaitForSingleObject(numThread, INFINITE);
-	
-
-	CloseHandle(numThread);
-	CloseHandle(alphaThread);
 
 	
 	return 0;
